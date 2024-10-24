19c9751ea1ab9e1973346a1af9baadf926f2afbb
Oresta-dev <developer@rand.com.mx>
2024-10-24 11:42:47 -0600

    feat: services section modify to animate

---
+++ v1.0/main/src/const/services.js
@@ -0,0 +1,51 @@
+export const Services = [
+    {
+        id: 1,
+        title: "Derecho administrativo",
+        description: "Cuenta con nuestros abogados especializados para salvaguardar tus intereses frente a riesgos patrimoniales. Te brindamos asesoría personalizada y soluciones a medida.",
+        buttonText: "Ver más"
+    },
+    {
+        id: 2,
+        title: "DERECHO CORPORATIVO",
+        description: "Soluciones personalizadas y efectivas para optimizar y proteger tu empresa.",
+        buttonText: "Ver más"
+    },
+    {
+        id: 3,
+        title: "DERECHO MERCANTIL",
+        description: "Nuestros abogados especializados ofrecen soluciones efectivas para proteger y optimizar tus operaciones comerciales.",
+        buttonText: "Ver más"
+    },
+    {
+        id: 4,
+        title: "DERECHO CIVIL",
+        description: "En derecho civil, contar con un equipo legal que ofrezca soluciones personalizadas es esencial. Nuestros abogados te brindan la orientación y protección que mereces.",
+        buttonText: "Ver más"
+    },
+    {
+        id: 5,
+        title: "SERVICIOS EN CONTABILIDAD",
+        description: "Ofrecemos soluciones contables precisas y eficientes, desde la contabilidad general hasta la declaración anual, para mantener tus finanzas en orden y conforme a la normativa.",
+        buttonText: "Ver más"
+    },
+    {
+        id: 6,
+        title: "DERECHO LABORAL",
+        description: "Ofrecemos la asesoría y protección necesarias para manejar cualquier situación laboral con confianza.",
+        buttonText: "Ver más"
+    },
+    {
+        id: 7,
+        title: "DERECHO FAMILIAR",
+        description: "Te brindamos soluciones personalizadas y eficaces para resolver tus asuntos familiares.",
+        buttonText: "Ver más"
+    },
+    {
+        id: 8,
+        title: "CONTRATOS",
+        description: "Contamos con soluciones precisas y efectivas para la elaboración, revisión y negociación de contratos.",
+        buttonText: "Ver más"
+    }
+]
+
